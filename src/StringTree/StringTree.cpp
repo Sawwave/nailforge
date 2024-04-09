1360e9b02320809d2e7fb76087791e499e0747d9
Tim Anderson <timanderson.um@gmail.com>
2024-04-09 12:36:43 -0600

    update string tree

--- src/StringTree/StringTree.cpp
+++ src/StringTree/StringTree.cpp
@@ -5,14 +5,15 @@
 #include <vector>
 
 namespace NailForge::StringTree {
-    int numSeeds = 0;
-    int verifiedSeeds = 0;
 
     struct StringTreeSparseScoreVectorEntry {
         StringTreeSparseScoreVectorEntry(const uint32_t modelPosition, const float score) :
-            modelPosition(modelPosition), score(score) {}
+            modelPosition(modelPosition), score(score), maxScore(score) {}
+        StringTreeSparseScoreVectorEntry(const uint32_t modelPosition, const float score, const float maxScore) :
+            modelPosition(modelPosition), score(score), maxScore(maxScore) {}
         uint32_t modelPosition;
         float score;
+        float maxScore;
     };
 
     struct StringTreeStackEntry {
@@ -22,29 +23,26 @@ namespace NailForge::StringTree {
     };
 
     void verifyDiagonalsPassingThreshold(const AwFmIndex* const fmIndex, const StringTreeStackEntry& stackEntry,
-        const uint32_t& thisSymbolModelPosition, const NailForge::SearchParams& params, const bool isReverseCompliment,
-        const FastaVector& fastaVector, const P7Hmm& phmm, const std::vector<float>& modelMatchScores, std::vector<AlignmentSeed>& seedList);
+        const uint32_t& thisSymbolModelPosition, const float maxScoreAlongDiagonal, const NailForge::SearchParams& params,
+        const bool isReverseCompliment, const FastaVector& fastaVector, const P7Hmm& phmm,
+        const std::vector<float>& modelMatchScores, std::vector<AlignmentSeed>& seedList);
 
 
     void findSeeds(const AwFmIndex* const fmIndex, const FastaVector& fastaVector, const P7Hmm& phmm,
         const uint32_t modelIdx, const std::vector<float>& matchScores, std::vector<AlignmentSeed>& seedList,
         const NailForge::SearchParams& params, const bool isReverseComplimentSearch) {
 
-
-        numSeeds = 0;
-        verifiedSeeds = 0;
+        const auto awfmBackwardStepFunction = phmm.header.alphabet == P7HmmReaderAlphabetAmino ?
+            awFmAminoIterativeStepBackwardSearch : awFmNucleotideIterativeStepBackwardSearch;
 
         const uint32_t nextDiagonalCellOffset = isReverseComplimentSearch ? 1 : -1;
-        const uint8_t symbolEncodingComplimentBitmask = isReverseComplimentSearch ? 0x03 : 0x00;
+        const uint8_t modelSymbolEncodingBitmask = isReverseComplimentSearch ? 0b11 : 0x00;
         const uint8_t alphabetSize = phmm.header.alphabet == P7HmmReaderAlphabetAmino ? 20 : 4;
         const uint32_t modelLength = phmm.header.modelLength;
 
         //set up the stacks that allow us to perform the recursion iteratively
         std::vector<StringTreeStackEntry> stack;
         stack.resize(params.maximumHitLength);
-        for (auto& entry : stack) {
-            entry.diagonalEntries.reserve(modelLength);
-        }
 
         Table::MaxExtensionTable maxExtensionTable =
             Table::MaxExtensionTable(modelLength, params.maximumHitLength, alphabetSize);
@@ -52,20 +50,20 @@ namespace NailForge::StringTree {
 
 
         //string tree loop begins here
-        int8_t currentDepth = 0;
+        int_fast8_t currentDepth = 0;
         stack[0].symbol = 0;
         while (currentDepth >= 0) {
             const uint8_t maxExtensionPositionsRemaining = (params.maximumHitLength - 1) - currentDepth;
             const auto& currentSymbol = stack[currentDepth].symbol;
             //character encoding is the character after complimenting (if doing reverse compliment search)
-            const uint8_t phmmSymbolIdx = currentSymbol ^ symbolEncodingComplimentBitmask;
+            const uint8_t phmmSymbolIdx = currentSymbol ^ modelSymbolEncodingBitmask;
             stack[currentDepth].diagonalEntries.clear();
 
             if (__builtin_expect(currentDepth == 0, false)) {
                 //create search range from first symbol
                 stack[0].searchRange.startPtr = fmIndex->prefixSums[currentSymbol];
                 stack[0].searchRange.endPtr = fmIndex->prefixSums[currentSymbol + 1] - 1;
-                for (uint32_t modelPosition = 0; modelPosition < modelLength;modelPosition++) {
+                for (uint32_t modelPosition = 0; modelPosition < modelLength; modelPosition++) {
                     //get the match score. note that we look up the non-complimented score, but we'll write down the complimented character later
                     const float matchScore = matchScores[(modelPosition * alphabetSize) + phmmSymbolIdx];
 
@@ -83,8 +81,8 @@ namespace NailForge::StringTree {
             else {
                 const auto& prevDiagonalEntries = stack[currentDepth - 1].diagonalEntries;
 
+                bool hasResolvedSearchRange = false;
                 for (const auto& diagonalEntry : prevDiagonalEntries) {
-                    bool hasResolvedSearchRange = false;
 
                     const uint32_t thisSymbolModelPosition = diagonalEntry.modelPosition + nextDiagonalCellOffset;
                     const float matchScore = matchScores[(thisSymbolModelPosition * alphabetSize) + phmmSymbolIdx];
@@ -92,30 +90,27 @@ namespace NailForge::StringTree {
                     if (accumulatedScore > 0.0f) {
                         const float bestExtension = maxExtensionTable.getMaxExtensionScore(thisSymbolModelPosition, maxExtensionPositionsRemaining);
                         const float bestScorePossible = accumulatedScore + bestExtension;
+                        
                         const bool accumulatedScorePassesThreshold = accumulatedScore >= params.mainDiagonalThresholdScore;
                         const bool bestExtensionPossiblePassesThreshold = bestScorePossible >= params.mainDiagonalThresholdScore;
 
                         //resolve the search range if we haven't yet.
                         if (__builtin_expect(!hasResolvedSearchRange && bestExtensionPossiblePassesThreshold, false)) {
                             stack[currentDepth].searchRange = stack[currentDepth - 1].searchRange;
-                            if (phmm.header.alphabet == P7HmmReaderAlphabetAmino) {
-                                awFmAminoIterativeStepBackwardSearch(fmIndex, &stack[currentDepth].searchRange, currentSymbol);
-                            }
-                            else {
-                                awFmNucleotideIterativeStepBackwardSearch(fmIndex, &stack[currentDepth].searchRange, currentSymbol);
-                            }
+                            awfmBackwardStepFunction(fmIndex, &stack[currentDepth].searchRange, currentSymbol);
                             hasResolvedSearchRange = true;
                         }
 
-                        if (__builtin_expect(accumulatedScorePassesThreshold, false)) {
-                            verifyDiagonalsPassingThreshold(fmIndex, stack[currentDepth], thisSymbolModelPosition,
+                        if (__builtin_expect(accumulatedScorePassesThreshold && (currentDepth < (params.maximumHitLength-1)), false)) {
+                            verifyDiagonalsPassingThreshold(fmIndex, stack[currentDepth], thisSymbolModelPosition, diagonalEntry.maxScore,
                                 params, isReverseComplimentSearch, fastaVector, phmm, matchScores, seedList);
 
                         }
                         //if we didn't pass the threshold but if fully extended this kmer might, add it to the
                         //next layer diagonal list
                         else if (bestExtensionPossiblePassesThreshold) {
-                            stack[currentDepth].diagonalEntries.emplace_back(thisSymbolModelPosition, accumulatedScore);
+                            stack[currentDepth].diagonalEntries.emplace_back(thisSymbolModelPosition, accumulatedScore,
+                                std::max(diagonalEntry.maxScore, accumulatedScore));
                         }
                     }
                 }
@@ -123,7 +118,7 @@ namespace NailForge::StringTree {
 
             //roll over the current symbol, and current depth if necessary
             bool exploreFurtherDownTree =
-                ((currentDepth + 1) < params.maximumHitLength) &&
+                ((currentDepth) < (params.maximumHitLength-1)) &&
                 stack[currentDepth].diagonalEntries.size() != 0 &&
                 (awFmSearchRangeLength(&stack[currentDepth].searchRange) != 0);
 
@@ -144,15 +139,15 @@ namespace NailForge::StringTree {
     }
 
     void verifyDiagonalsPassingThreshold(const AwFmIndex* const fmIndex, const StringTreeStackEntry& stackEntry,
-        const uint32_t& thisSymbolModelPosition, const NailForge::SearchParams& params, const bool isReverseCompliment,
-        const FastaVector& fastaVector, const P7Hmm& phmm, const std::vector<float>& modelMatchScores, std::vector<AlignmentSeed>& seedList) {
+        const uint32_t& thisSymbolModelPosition, const float maxScoreAlongDiagonal, const NailForge::SearchParams& params,
+        const bool isReverseCompliment, const FastaVector& fastaVector, const P7Hmm& phmm,
+        const std::vector<float>& modelMatchScores, std::vector<AlignmentSeed>& seedList) {
         uint64_t searchRangeLength = awFmSearchRangeLength(&stackEntry.searchRange);
         //if there are way too many sequence hits, it's probably in some repetitive data, and we shouldn't report it.
-        const float hitsPerMillionPositions = searchRangeLength / ((float)fastaVector.sequence.count / 1e6);
+        const double hitsPerMillionPositions = (double)searchRangeLength / (((double)fastaVector.sequence.count / (double)1e6));
         if (hitsPerMillionPositions > params.maxSeqHitsPerMillion) {
             return;
         }
-
         for (uint64_t bwtPosition = stackEntry.searchRange.startPtr;
             bwtPosition <= stackEntry.searchRange.endPtr;bwtPosition++) {
             uint64_t globalSequencePosition, localSequencePosition, sequenceIdx;
@@ -173,12 +168,11 @@ namespace NailForge::StringTree {
             }
 
             bool seedIsVerified = SeedExtension::verifySeedViaExtension(localSequencePosition,
-                thisSymbolModelPosition, sequenceIdx, params, isReverseCompliment, fastaVector, phmm, modelMatchScores);
-            numSeeds++;
+                thisSymbolModelPosition, maxScoreAlongDiagonal, sequenceIdx, params,
+                isReverseCompliment, fastaVector, phmm, modelMatchScores);
             if (seedIsVerified) {
                 seedList.emplace_back(localSequencePosition, thisSymbolModelPosition, sequenceIdx);
 
-                verifiedSeeds++;
             }
         }
     }
