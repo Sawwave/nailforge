9127c61cbe9782d3b8aaa1e9383b2139c39b54bf
Tim Anderson <timanderson.um@gmail.com>
2024-04-09 12:48:41 -0600

    add docstrings

--- src/Alphabet/Alphabet.hpp
+++ src/Alphabet/Alphabet.hpp
@@ -14,7 +14,15 @@ namespace NailForge {
         Amino = 2
     };
 
+    /// @brief determines if the alphabet in the phmm list matches the fm index. Only checks the first model of the phmm list.
+    /// @param phmmList list of phmms
+    /// @param fmIndex fm index
+    /// @return true if the alphabets match 
     bool alphabetsMatch(const P7HmmList& phmmList, const AwFmIndex* fmIndex);
+
+    /// @brief finds the cardinality of the given alphabet
+    /// @param alphabet  alphabet to check
+    /// @return 20 for amino acid, or 4 for DNA/RNA
     uint32_t getAlphabetSize(const NailForge::Alphabet alphabet);
 
 }
