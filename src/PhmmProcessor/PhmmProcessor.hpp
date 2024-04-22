6c28dc2f2de4f0c3417f15042447cf36d82fa459
Tim Anderson <timanderson.um@gmail.com>
2024-04-22 17:13:17 -0600

    add block comments

--- src/PhmmProcessor/PhmmProcessor.hpp
+++ src/PhmmProcessor/PhmmProcessor.hpp
@@ -5,20 +5,42 @@
 #include "../nailforge.hpp"
 
 namespace NailForge::PhmmProcessor {
-    //Converts the phmm match scores to bits represented by floats
+
+    /// @brief converts the negative nat log likelihood scores in the phmm into bits scores for use in alignment.
+    /// @param phmm phmm to convert score
+    /// @return flattened float vector of scores. Score for symbol s at position p is located at vector idx [p * alphabetCardinality + s]
     std::vector<float> toFloatMatchScores(const P7Hmm& phmm);
 
-    //finds a phmm's threshold value for a given p-value using the model's MAXL field
+    /// @brief finds the threshold score necessary to meet a given p-value. Will use the hmm MAXL property if it exists.
+    /// @param phmm phmm to find the threshold for
+    /// @param sequenceLength length of the target sequence. this will be used if the MAXL property is missing.
+    /// @param pValue p-value that will be hit at the returned score.
+    /// @return score, in bits, necessary to hit the given p-value.
+    float findThreshold(const P7Hmm& phmm, const uint64_t sequenceLength, const float pValue);
+
+    /// @brief finds the threshold score necessary to hit to meet the given p-value for a nucleotide phmm.
+    /// @param phmm phmm to find the threshold for
+    /// @param pValue p-value that will be hit at the returned score.
+    /// @return score, in bits, necessary to hit the given p-value.
     float findThresholdForNucleotide(const P7Hmm& phmm, const float pValue);
 
-    //find a phmm's threshold value for a given p-value using the target sequence length
+    /// @brief finds the threshold score necessary to hit to meet the given p-value for an amino phmm.
+    /// @param phmm phmm to find the threshold for
+    /// @param sequenceLength length of the target sequence. Nucleotide Phmms use its MAXL property, but since AA models don't have this property, use the target sequence length.
+    /// @param pValue p-value that will be hit at the returned score.
+    /// @return score, in bits, necessary to hit the given p-value.
     float findThresholdForAmino(const P7Hmm& phmm, const uint64_t sequenceLength, const float pValue);
 
-    //The inverse gumbel survival function, aka, what score is needed to hit the given p-value?
+    /// @brief finds the inverse survival of the gumbel function
+    /// @param lambda lambda value from the STATS LOCAL MSV line of the model
+    /// @param mu mu value from the STATS LOCAL MSV line of the model
+    /// @param pValue p-value to hit
+    /// @return  inverse survival value
     float gumbelInverseSurvival(const float lambda, const float mu, const float pValue);
 
-
-    //finds the number of match scores per phmm position, aka, the cardinality of phmm's alphabet.
+    /// @brief finds the number of match scores in the phmm, defined as model length * alphabet cardinality
+    /// @param phmm model to query
+    /// @return length of the model multiplied by the size of the model's alphabet
     uint64_t getNumMatchScores(const P7Hmm& phmm);
 
 };
