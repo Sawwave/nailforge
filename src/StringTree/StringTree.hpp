c0464f7cb889f34261a1d7437e2fa056cf17ea6b
Tim Anderson <timanderson.um@gmail.com>
2024-04-22 19:08:51 -0600

    add block comment documentation

--- src/StringTree/StringTree.hpp
+++ src/StringTree/StringTree.hpp
@@ -5,6 +5,7 @@
 
 namespace NailForge::StringTree {
 
+    /// @brief Data bundle for constant data for a full findSeeds invocation
     struct Context {
         Context(const AwFmIndex& fmIndex, const FastaVector& fastaVector,
             const P7Hmm& phmm, const std::vector<float>& matchScores, const NailForge::SearchParams& searchParams,
@@ -21,6 +22,8 @@ namespace NailForge::StringTree {
         const bool isReverseCompliment;
     };
 
+
+    /// @brief Describes a position in model/sequence space where a StringTree hit occurrs.
     struct HitPosition {
         HitPosition(const uint64_t sequenceIdx, const uint64_t localSequencePosition,
             const uint32_t modelPosition, const uint8_t hitLength) noexcept :
@@ -32,8 +35,10 @@ namespace NailForge::StringTree {
         const uint8_t hitLength;
     };
 
-
-    void findSeeds(const StringTree::Context& context, std::vector<AlignmentSeed>& seedList);
+    /// @brief finds seeds, first with the string tree, and then validates them using the SeedExtension module, and adds verified seeds to the seed list.
+    /// @param context Context of the search
+    /// @param seedList data structure to append hits to.
+    void findSeeds(const StringTree::Context& context, std::vector<AlignmentSeed>& seedList) noexcept;
 }
 
 
